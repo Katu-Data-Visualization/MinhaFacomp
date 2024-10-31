7394c0d02d55b76480c150bdf3662f8a0212f079
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-10-31 07:40:13 -0300

    feat: add NavBar Component

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -1,9 +1,35 @@
 import "./LandingPage.css";
 
+import logoFACOMPsvg from "../MinhaFacompV2Components/Assets/imgs/logo-facomp-svg.svg";
+import { Moon } from "@phosphor-icons/react";
+
 const LandingPage = () => {
   return (
     <div className="container-LandingPage">
-      <h1>Landing Page</h1>
+      {/* NAVIGATION */}
+      <nav className="nav-container">
+        <div className="logo-container">
+          <figure className="logo-FACOMP">
+            <a href="https://computacao.ufpa.br/" target="_blank">
+              <img src={logoFACOMPsvg} alt="Logo da FACOMP" />
+            </a>
+          </figure>
+        </div>
+        <div className="nav-items">
+          <div className="nav-links">
+            <a href="">Início</a>
+            <a href="">Sobre</a>
+            <a href="">Funcionalidades</a>
+            <a href="">Parceiros</a>
+          </div>
+          <div className="nav-buttons">
+            <button className="button-access">Acessar Plataforma</button>
+            <button className="button-toggle-theme">
+              <Moon className="icon-toggle-theme" size={24} />
+            </button>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
