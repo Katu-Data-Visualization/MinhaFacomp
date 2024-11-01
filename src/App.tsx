ad3545f762d3bebb0d29f85835cde6a6d94de08e
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-11-01 18:26:57 -0300

    fix: add correct basename

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MinhaFacomp from "./MinhaFacompV2Components/MinhaFacomp";
 import LandingPage from "./LandingPage/LandingPage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/home",
+      element: <MinhaFacomp />,
+    },
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+  ],
   {
-    path: "/home",
-    element: <MinhaFacomp />,
-  },
-  {
-    path: "/",
-    element: <LandingPage />,
-  },
-]);
+    basename: "/d/minhafacomp/",
+  }
+);
 
 export default function App() {
   return <RouterProvider router={router} />;
